519d1c0b97d1c293237a6f6c62fc410af945726c
verox- <verox.averre@gmail.com>
2018-05-04 21:16:07 +0100

    Still trying to cure ADD.

--- src/worker.role.js
+++ src/worker.role.js
@@ -1,23 +1,29 @@
 var roleWorker = {
 
-    init: function () {
-        memory.archtype = "worker";
-        memory.role = "miner";
-        memory.level = 1;
+    init: function (creep) {
+        creep.memory.archtype = "worker";
+        creep.memory.role = "miner";
+        creep.memory.level = 1;
+        creep.memory.eating = false;
     },
 
     /** @param {Creep} creep **/
     run: function(parameters) {
         var creep = parameters.creep;
-        if(creep.carry.energy <= 0) {
+        if(creep.carry.energy <= 0 || creep.memory.eating) {
             var sources = creep.room.find(FIND_SOURCES);
             var harvestResult = creep.harvest(sources[0]);
             if(harvestResult === ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0],{visualizePathStyle: {stroke: '#ffffff'}});
                 creep.say("MH");
             }
+            else if (harvestResult === ERR_FULL)
+            {
+                creep.memory.eating = false;
+            }
             else if (harvestResult === OK) {
                 creep.say("HH");
+                creep.memory.eating = true;
             }
         }
         else if (Game.spawns['Seed'].energy >= Game.spawns['Seed'].energyCapacity) {
