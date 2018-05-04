10b6e831017bfdf5fd52ca6d332cf2b5dee40bd6
verox- <verox.averre@gmail.com>
2018-05-05 00:26:06 +0100

    Removed init

--- src/factory.controller.js
+++ src/factory.controller.js
@@ -36,6 +36,5 @@ module.exports = {
     spawnCreep: function (spawn, role) {
         let name = Math.random().toString(36).substring(7);
         spawn.createCreep([WORK, MOVE, CARRY], name, {role: role});
-        Game.creeps[name].init();
     }
 };
