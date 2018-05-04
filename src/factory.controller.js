b6fce9c8233ba6dcf51c9db0b80fc9cabaef718c
verox- <verox.averre@gmail.com>
2018-05-04 20:20:38 +0100

    Fixed creep capacity to actually use creeps not spawns.

--- src/factory.controller.js
+++ src/factory.controller.js
@@ -19,7 +19,7 @@ module.exports = {
         for (var i in Game.spawns) {
             var spawn = Game.spawns[i];
 
-            if (spawn.spawning || Game.spawns.length >= Memory.creepCapacity) return;
+            if (spawn.spawning || Game.creeps.length >= Memory.creepCapacity) return;
 
             this.spawnCreep(spawn, "worker");
         }
