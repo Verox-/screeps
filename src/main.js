7a6438d3af9f5dd99fc698c97072396d215aeb6e
verox- <verox.averre@gmail.com>
2018-05-07 13:37:21 +0100

    Quick and dirty tower code

--- src/main.js
+++ src/main.js
@@ -11,4 +11,27 @@ module.exports.loop = function () {
 
     // Think the creeps
     creepC.run();
+
+    /**
+     * Quick and dirty tower code for now.
+     */
+    let towers = _.filter(Game.structures, (structure) => structure.structureType === STRUCTURE_TOWER);
+
+    towers.forEach((tower) => {
+        if(tower) {
+            let closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+
+            if(closestHostile) {
+                tower.attack(closestHostile);
+            }
+            let closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: (structure) => (structure.structureType === STRUCTURE_CONTAINER) || structure.hits < (structure.hitsMax*0.0003)
+            });
+
+            if(closestDamagedStructure) {
+                tower.repair(closestDamagedStructure);
+            }
+
+        }
+    })
 };
