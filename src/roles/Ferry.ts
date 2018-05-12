0178d99a6ca85625127a4ed8eb05eec99dcc5018
verox- <verox.averre@gmail.com>
2018-05-12 18:04:34 +0100

    Ferry will now deposit in the storage container if all other containers are full.

--- src/roles/Ferry.ts
+++ src/roles/Ferry.ts
@@ -110,7 +110,12 @@ export class RoleFerry implements Role {
             }
         });
 
-        if (targets.length <= 0) return;
+        if (targets.length <= 0) {
+            if (creep.room.storage === undefined)
+                return;
+            else
+                targets[0] = creep.room.storage;
+        }
 
         if (creep.transfer(targets[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
             creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
